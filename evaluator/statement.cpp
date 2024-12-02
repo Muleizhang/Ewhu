d08ea0c31aec8f901c229703d02fd791376318cd
mulei <muleizh@outlook.com>
2024-12-02 13:35:06 +0800

    fix while

--- evaluator/statement.cpp
+++ evaluator/statement.cpp
@@ -7,7 +7,7 @@ std::shared_ptr<Object> Evaluator::eval_statement_block(const std::vector<std::s
     for (auto &stat : stmts)
     {
         result = eval(stat, temp_scope);
-        if (is_error(result) || result->type() == Object::OBJECT_BREAK||result->type()==Object::OBJECT_CONTINUE)
+        if (is_error(result) || result->type() == Object::OBJECT_BREAK || result->type() == Object::OBJECT_CONTINUE)
         {
             break;
         }
@@ -52,59 +52,70 @@ std::shared_ptr<Object> Evaluator::eval_while_statement(const std::shared_ptr<Ex
 {
     std::shared_ptr<Object> result(new Ob_Null());
     auto s = eval(exp, scp);
+
     switch (s->type())
     {
     case Object::OBJECT_BOOLEAN:
-        while (std::dynamic_pointer_cast<Ob_Boolean>(eval(exp, scp))->m_value)
+        if (std::dynamic_pointer_cast<Ob_Boolean>(s)->m_value)
         {
-            result = eval(true_statement, scp);
-            if (result->type() == Object::OBJECT_BREAK)
+            do
             {
-                result.reset(new Ob_Null());
-                return result;
-            }
-            if (result->type() == Object::OBJECT_CONTINUE)
-            {
-                result.reset(new Ob_Null);
-                continue;
-            }
+                result = eval(true_statement, scp);
+                if (result->type() == Object::OBJECT_BREAK)
+                {
+                    result.reset(new Ob_Null());
+                    return result;
+                }
+                if (result->type() == Object::OBJECT_CONTINUE)
+                {
+                    result.reset(new Ob_Null);
+                    continue;
+                }
+            } while (std::dynamic_pointer_cast<Ob_Boolean>(eval(exp, scp))->m_value);
         }
         break;
 
     case Object::OBJECT_INTEGER:
-        while (std::dynamic_pointer_cast<Ob_Integer>(eval(exp, scp))->m_value)
+        if (std::dynamic_pointer_cast<Ob_Integer>(s)->m_value)
         {
-            result = eval(true_statement, scp);
-            if (result->type() == Object::OBJECT_BREAK)
+
+            do
             {
-                result.reset(new Ob_Null());
-                return result;
-            }
-            if (result->type() == Object::OBJECT_CONTINUE)
-            {
-                result.reset(new Ob_Null);
-                continue;
-            }
+                result = eval(true_statement, scp);
+                if (result->type() == Object::OBJECT_BREAK)
+                {
+                    result.reset(new Ob_Null());
+                    return result;
+                }
+                if (result->type() == Object::OBJECT_CONTINUE)
+                {
+                    result.reset(new Ob_Null);
+                    continue;
+                }
+            } while (std::dynamic_pointer_cast<Ob_Integer>(eval(exp, scp))->m_value);
         }
         break;
 
     case Object::OBJECT_FRACTION:
-        while (std::dynamic_pointer_cast<Ob_Fraction>(eval(exp, scp))->num)
+        if (std::dynamic_pointer_cast<Ob_Fraction>(s)->num)
         {
-            result = eval(true_statement, scp);
-            if (result->type() == Object::OBJECT_BREAK)
+            do
             {
-                result.reset(new Ob_Null());
-                return result;
-            }
-            if (result->type() == Object::OBJECT_CONTINUE)
-            {
-                result.reset(new Ob_Null);
-                continue;
-            }
+                result = eval(true_statement, scp);
+                if (result->type() == Object::OBJECT_BREAK)
+                {
+                    result.reset(new Ob_Null());
+                    return result;
+                }
+                if (result->type() == Object::OBJECT_CONTINUE)
+                {
+                    result.reset(new Ob_Null);
+                    continue;
+                }
+            } while (std::dynamic_pointer_cast<Ob_Fraction>(eval(exp, scp))->num);
         }
-
         break;
+
     default:
         new_error("Evaluator::eval_while_statement: unknown type: %d", s->type());
         break;
