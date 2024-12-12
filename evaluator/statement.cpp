ec57dba71bba263d8192835f355cf95873530b2a
mulei <muleizh@outlook.com>
2024-12-12 18:38:53 +0800

    fix if

--- evaluator/statement.cpp
+++ evaluator/statement.cpp
@@ -2,34 +2,24 @@
 
 std::shared_ptr<Object> Evaluator::eval_statement_block(const std::vector<std::shared_ptr<Node>> &stmts, Scope &scp)
 {
-    std::shared_ptr<Object> result(new Ob_Null());
+    std::shared_ptr<Object> result = nullptr;
 
-    Scope temp_scope; // 局部作用域
+    Scope temp_scope;
     temp_scope.father = &scp;
     for (auto &stat : stmts)
     {
         result = eval(stat, temp_scope);
         if (result)
         {
-            if (result->type() == Object::OBJECT_BREAK || result->type() == Object::OBJECT_CONTINUE)
-            {
-                break;
-            }
-            if (result->type() == Object::OBJECT_RETURN || is_error(result))
+            if (result->type() == Object::OBJECT_BREAK || result->type() == Object::OBJECT_CONTINUE ||
+                result->type() == Object::OBJECT_RETURN || is_error(result))
             {
                 return result;
             }
         }
     }
 
-    // for (auto it : temp_scope.m_var)
-    // {
-    //     auto org_it = scp.m_var.find(it.first);
-    //     if (org_it != scp.m_var.end())
-    //         scp.m_var[it.first] = it.second;
-    // }
-
-    return nullptr;
+    return result;
 }
 
 std::shared_ptr<Object> Evaluator::eval_function_block(const std::shared_ptr<Node> function,
@@ -76,9 +66,7 @@ std::shared_ptr<Object> Evaluator::eval_if_statement(const std::shared_ptr<Node>
     if (eval(exp, scp)->m_int)
     {
         auto result = eval(true_statement, scp);
-        if (result)
-            return result;
-        return nullptr;
+        return result;
     }
     return nullptr;
 }
